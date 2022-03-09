53e42474edebf59641ab769cd5c9bd7f5db5e6a7
Andrew Moore <andrewdmoore84@gmail.com>
2022-03-09 11:33:34 -0500

    Add Model class definition
    Add contact; get all contacts; delete contact; search contacts functionality added, working with the API to make changes in the database

---
+++ public/javascripts/contact_manager.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', () => {
+  class Model {
+    async addContact(contactInfo) {
+      const ADD_CONTACT_URL = '/api/contacts/';
+      const HEADERS = {
+        'Content-Type': 'application/json',
+      };
+
+      try {
+        return await fetch(ADD_CONTACT_URL, {
+          method: 'POST',
+          headers: HEADERS,
+          body: JSON.stringify(contactInfo),
+        }).then(response => response.json());
+      } catch (error) {
+        return {error: error};
+      }
+    }
+
+    async getAllContacts() {
+      const ALL_CONTACTS_URL = '/api/contacts';
+
+      return await fetch(ALL_CONTACTS_URL).then(response => response.json());
+    }
+
+    async deleteContact(id) {
+      const DELETE_CONTACT_URL = `/api/contacts/${id}`;
+
+      try {
+        return await fetch(DELETE_CONTACT_URL, {
+          method: 'DELETE',
+        }).then(response => response.status);
+      } catch (error) {
+        return {error: error};
+      }
+    }
+
+    async searchContacts(searchString) {
+      const allContacts = await this.getAllContacts();
+
+      return allContacts.filter(contact => contact.full_name.toLowerCase().includes(searchString.toLowerCase()));
+    }
+  }
+
+  class View {
+
+  }
+
+  class Controller {
+    #model;
+    #view;
+
+    constructor() {
+      this.#view = new View();
+      this.#model = new Model();
+    }
+  }
+
+  let model = new Model();
+
+  // model.addContact({
+  //   full_name: "Scooby-Doo",
+  //   email: "email@addy.com",
+  //   phone_number: "119",
+  //   tags: "investigator, programmer",
+  // }).then(response => console.log(response));
+
+  // model.getAllContacts().then(response => console.log(`response is: ${response.forEach(object => console.log(object))}`));
+
+  // model.deleteContact(6).then(response => console.log(`delete status: ${response}`));
+
+  model.searchContacts('I').then(response => response.forEach(contact => console.log(contact)));
+
+  model.searchContacts('n').then(response => response.forEach(contact => console.log(contact)));
+});
